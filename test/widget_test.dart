29410e10573b60974203a0e2e08699a1fb4d9ec7
Melvyn <sngguojie@gmail.com>
2022-11-03 18:16:31 -0700

    [Test] Update to run according to Example App and pass

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,24 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 
 import 'package:argon_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Loads and navigates from Onboarding to Home', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // On onboarding page
+    expect(find.text('GET STARTED'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Taps button
+    await tester.tap(find.text('GET STARTED'));
+    await mockNetworkImagesFor(() => tester.pumpAndSettle());
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Expect on Home page
+    expect(find.text("Home"), findsOneWidget);
   });
 }
